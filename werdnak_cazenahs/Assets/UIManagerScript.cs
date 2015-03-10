899f413263a7d1cb61dff3f87fe5bbf43a258583
Schwarzy1 <Schwarzyz@gmail.com>
2015-03-10 16:03:57 -0500

    UIManagerHandler
    Handles buttons

---
+++ werdnak_cazenahs/Assets/UIManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartGame () {
+		Application.LoadLevel("scene_one");
+	}
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+	public void Credits()
+	{
+	}
+	public void LevelSelect()
+	{
+	}
+	public void Options()
+	{
+	}
+}
