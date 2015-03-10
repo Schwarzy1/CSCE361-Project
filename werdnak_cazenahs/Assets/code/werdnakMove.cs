2293cb1c91f8c52c560792670df92be006fa9095
zboone1995 <zboone1995@gmail.com>
2015-03-10 15:53:37 -0500

    Movement and Collisions
    Characters now move based on input keys and will collide with each other
    and the room walls.

--- werdnak_cazenahs/Assets/code/werdnakMove.cs
+++ werdnak_cazenahs/Assets/code/werdnakMove.cs
@@ -1,30 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class cazenahsMove : MonoBehaviour {
+public class werdnakMove : MonoBehaviour {
 	float speed = .1f;
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if(Input.GetKey(KeyCode.UpArrow)){
+		if(Input.GetKey(KeyCode.W)){
 			transform.Translate(0,speed,0);
 		}
-		if(Input.GetKey(KeyCode.DownArrow)){
+		if(Input.GetKey(KeyCode.S)){
 			transform.Translate(0,(-1)*speed,0);
 		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
+		if(Input.GetKey(KeyCode.A)){
 			transform.Translate((-1)*speed,0,0);
 			transform.localScale = (new Vector3(-1,1,1));
 		}
-		if(Input.GetKey(KeyCode.RightArrow)){
+		if(Input.GetKey(KeyCode.D)){
 			transform.Translate(speed,0,0);
 			transform.localScale = (new Vector3(1,1,1));
 		}
-		//GetComponent<Rigidbody2D> ().velocity = new Vector3 (1, 1, 1);
 	}
 }
