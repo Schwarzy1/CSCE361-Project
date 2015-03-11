7f1d8df21d7dc30c4081ba8fa9c137fe40621990
Schwarzy1 <Schwarzyz@gmail.com>
2015-03-11 15:05:08 -0500

    Created Menu Scenes
    Added scenes for menu buttons to link to, with back buttons allowing
    navigation, pretty much finished credits menu

--- werdnak_cazenahs/Assets/UIManagerScript.cs
+++ werdnak_cazenahs/Assets/UIManagerScript.cs
@@ -7,17 +7,28 @@ public class UIManagerScript : MonoBehaviour {
 	public void StartGame () {
 		Application.LoadLevel("scene_one");
 	}
+
+	public void CharSelect(){
+		Application.LoadLevel ("CharacterSelect");
+	}
+
 	public void Exit()
 	{
 		Application.Quit ();
 	}
-	public void Credits()
-	{
+	public void Credits(){
+		Application.LoadLevel ("Credits");
 	}
-	public void LevelSelect()
-	{
+	public void LevelSelect(){
+		Application.LoadLevel ("LevelSelect");
 	}
-	public void Options()
-	{
+	public void Options(){
+		Application.LoadLevel ("Options");
+	}
+
+	public void Back(){
+	
+		Application.LoadLevel ("Menus");
+	
 	}
 }
