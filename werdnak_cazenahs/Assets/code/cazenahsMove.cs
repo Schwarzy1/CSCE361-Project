79130d1899108be25d031bff8f02fe50f03b3bbd
enahsthezombie <enahsnalob@gmail.com>
2015-03-11 23:05:52 -0500

    fixed animations, changed werd walk speed to .08

--- werdnak_cazenahs/Assets/code/cazenahsMove.cs
+++ werdnak_cazenahs/Assets/code/cazenahsMove.cs
@@ -3,27 +3,33 @@ using System.Collections;
 
 public class cazenahsMove : MonoBehaviour {
 	float speed = .1f;
+	Animator animator;
 	// Use this for initialization
 	void Start () {
-
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		
+		animator.SetFloat("Speed", 0);
 		if(Input.GetKey(KeyCode.UpArrow)){
 			transform.Translate(0,speed,0);
+			animator.SetFloat("Speed", 1);
 		}
 		if(Input.GetKey(KeyCode.DownArrow)){
 			transform.Translate(0,(-1)*speed,0);
+			animator.SetFloat("Speed", 1);
 		}
 		if(Input.GetKey(KeyCode.LeftArrow)){
 			transform.Translate((-1)*speed,0,0);
 			transform.localScale = (new Vector3(-1,1,1));
+			animator.SetFloat("Speed", 1);
 		}
 		if(Input.GetKey(KeyCode.RightArrow)){
 			transform.Translate(speed,0,0);
 			transform.localScale = (new Vector3(1,1,1));
+			animator.SetFloat("Speed", 1);
 		}
 		//GetComponent<Rigidbody2D> ().velocity = new Vector3 (1, 1, 1);
 	}
--- werdnak_cazenahs/Assets/code/werdnakMove.cs
+++ werdnak_cazenahs/Assets/code/werdnakMove.cs
@@ -2,28 +2,34 @@
 using System.Collections;
 
 public class werdnakMove : MonoBehaviour {
-	float speed = .1f;
+	float speed = .08f;
+	Animator animator;
 	// Use this for initialization
 	void Start () {
-		
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+
+		animator.SetFloat("Speed", 0);
 		if(Input.GetKey(KeyCode.W)){
 			transform.Translate(0,speed,0);
+			animator.SetFloat("Speed", 1);
 		}
 		if(Input.GetKey(KeyCode.S)){
 			transform.Translate(0,(-1)*speed,0);
+			animator.SetFloat("Speed", 1);
 		}
 		if(Input.GetKey(KeyCode.A)){
 			transform.Translate((-1)*speed,0,0);
 			transform.localScale = (new Vector3(-1,1,1));
+			animator.SetFloat("Speed", 1);
 		}
 		if(Input.GetKey(KeyCode.D)){
 			transform.Translate(speed,0,0);
 			transform.localScale = (new Vector3(1,1,1));
+			animator.SetFloat("Speed", 1);
 		}
 	}
 }
