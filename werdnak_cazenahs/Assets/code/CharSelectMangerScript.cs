d1f1cd2ea3ef9caa98e0aaa6e0a93866cb30e81d
Schwarzy1 <Schwarzyz@gmail.com>
2015-03-12 15:59:40 -0500

    Fixed Menu System
    Moved level select, working on char select

---
+++ werdnak_cazenahs/Assets/code/CharSelectMangerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharSelectMangerScript : MonoBehaviour {
+	public bool isWerdRed=false;
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
+		GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,5);
+		GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
+		GameObject.Find ("werd_b_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.A)) {
+			GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
+			GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
+			GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
+			GameObject.Find ("werd_b_highlight").transform.position.Set((float)216.25,(float)-32.32,5);
+			isWerdRed=false;
+		}
+		if (Input.GetKey (KeyCode.B)) {
+			GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
+			GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
+			GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
+			GameObject.Find ("werd_b_highlight").transform.position.Set((float)216.25,(float)-32.32,5);
+			isWerdRed=true;
+		}
+	}
+}
--- werdnak_cazenahs/Assets/code/UIManagerScript.cs
+++ werdnak_cazenahs/Assets/code/UIManagerScript.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
+	public string lastLevel="Menus";
+
 
 	// Use this for initialization
-	public void StartGame () {
-		Application.LoadLevel("scene_one");
+	public void StartGame(){
+		Application.LoadLevel ("scene_one");
 	}
+
+	public void StartGame (string selectedLevel) {
+		Application.LoadLevel(selectedLevel);
+	}
+
 
 	public void CharSelect(){
 		Application.LoadLevel ("CharacterSelect");
+		lastLevel = "CharacterSelect";
 	}
 
 	public void Exit()
@@ -18,12 +26,15 @@ public class UIManagerScript : MonoBehaviour {
 	}
 	public void Credits(){
 		Application.LoadLevel ("Credits");
+		lastLevel = "Credits";
 	}
 	public void LevelSelect(){
 		Application.LoadLevel ("LevelSelect");
+		lastLevel= "LevelSelect";
 	}
 	public void Options(){
 		Application.LoadLevel ("Options");
+		lastLevel = "Options";
 	}
 
 	public void Back(){
@@ -31,4 +42,14 @@ public class UIManagerScript : MonoBehaviour {
 		Application.LoadLevel ("Menus");
 	
 	}
+
+	public void BackLevelSel(){
+		Application.LoadLevel ("CharacterSelect");
+	}
+	public void LevelOne(){
+
+		StartGame ("scene_one");
+
+	}
+
 }
