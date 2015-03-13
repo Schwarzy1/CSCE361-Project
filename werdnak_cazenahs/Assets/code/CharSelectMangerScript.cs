38d6e973c2f7de4057cb3d7db972ec96618c10e1
enahsthezombie <enahsnalob@gmail.com>
2015-03-13 14:32:29 -0500

    updated the character select possitions

--- werdnak_cazenahs/Assets/code/CharSelectMangerScript.cs
+++ werdnak_cazenahs/Assets/code/CharSelectMangerScript.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 public class CharSelectMangerScript : MonoBehaviour {
 	public bool isWerdRed=false;
+
 	// Use this for initialization
 	void Start () {
-		GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
-		GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,5);
-		GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
-		GameObject.Find ("werd_b_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
-	
+		//GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
+		//GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,5);
+		//GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
+		//GameObject.Find ("werd_b_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKey (KeyCode.A)) {
-			GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
-			GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
-			GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
-			GameObject.Find ("werd_b_highlight").transform.position.Set((float)216.25,(float)-32.32,5);
+		if (Input.GetKeyDown(KeyCode.A)) {
+			GameObject.Find("caz_b_highlight").transform.localPosition.Set(0,0, -5);
+			GameObject.Find ("werd_r_highlight").transform.localPosition.Set(0,0,-5);
+			GameObject.Find("caz_r_highlight").transform.localPosition.Set(0,0, 5);
+			GameObject.Find ("werd_b_highlight").transform.localPosition.Set(0,0,5);
 			isWerdRed=false;
 		}
-		if (Input.GetKey (KeyCode.B)) {
+		if (Input.GetKeyDown(KeyCode.B)) {
 			GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
 			GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
 			GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
