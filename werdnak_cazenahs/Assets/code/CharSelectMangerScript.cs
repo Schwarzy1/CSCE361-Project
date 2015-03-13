e86414b0b7d9966b40e0ed8b25714f945f6d95ee
Schwarzy1 <Schwarzyz@gmail.com>
2015-03-13 15:42:52 -0500

    Fixed Char Select

--- werdnak_cazenahs/Assets/code/CharSelectMangerScript.cs
+++ werdnak_cazenahs/Assets/code/CharSelectMangerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class CharSelectMangerScript : MonoBehaviour {
-	public bool isWerdRed=false;
+	public bool isWerdRed=true;
 
 	// Use this for initialization
 	void Start () {
@@ -14,19 +14,20 @@ public class CharSelectMangerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKeyDown(KeyCode.A)) {
-			GameObject.Find("caz_b_highlight").transform.localPosition.Set(0,0, -5);
+		if (Input.GetKeyDown(KeyCode.A) && !isWerdRed) {
+			GameObject.Find("caz_b_highlight").transform.Translate(0,0, -5);
 			GameObject.Find ("werd_r_highlight").transform.localPosition.Set(0,0,-5);
 			GameObject.Find("caz_r_highlight").transform.localPosition.Set(0,0, 5);
 			GameObject.Find ("werd_b_highlight").transform.localPosition.Set(0,0,5);
 			isWerdRed=false;
 		}
-		if (Input.GetKeyDown(KeyCode.B)) {
+		if (Input.GetKeyDown(KeyCode.B) && isWerdRed) {
 			GameObject.Find("caz_b_highlight").transform.position.Set((float)225.16,(float)-32.82, -5);
 			GameObject.Find ("werd_r_highlight").transform.position.Set((float)216.25,(float)-32.32,-5);
 			GameObject.Find("caz_r_highlight").transform.position.Set((float)225.16,(float)-32.82, 5);
 			GameObject.Find ("werd_b_highlight").transform.position.Set((float)216.25,(float)-32.32,5);
 			isWerdRed=true;
+
 		}
 	}
 }
