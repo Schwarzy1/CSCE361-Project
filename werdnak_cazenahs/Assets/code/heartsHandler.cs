8169f5c845990f515fca325774b3a09f5c412839
enahsthezombie <enahsnalob@gmail.com>
2015-03-16 00:44:35 -0500

    added the HUD elements and changed room enter pos

---
+++ werdnak_cazenahs/Assets/code/heartsHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heartsHandler : MonoBehaviour {
+
+	int health = 6;
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.X)){
+			TakeDamage(1);
+		}
+	}
+
+	void TakeDamage(int damage){
+		health -= damage;
+		if(health >= 6){
+			health = 6;
+		}else if(health <= 0){
+			health = 0;
+		}
+		animator.SetInteger("health", health);
+	}
+
+}
