f4af24578e5af18cea02c0bebcdbb9fee5e9dfe3
zboone1995 <zboone1995@gmail.com>
2015-03-16 00:02:53 -0500

    Melee Robot Movement
    Added melee robot movement AI so that the robot will move in a random
    matter until a character enters its region.  It then converges on that
    character.  Also added commenting and variable name changes to blobMove
    code to be more clear.

--- werdnak_cazenahs/Assets/code/blobMove.cs
+++ werdnak_cazenahs/Assets/code/blobMove.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class blobMove : MonoBehaviour {
 	float speed = .01f;
-	float r = 0;
-	float s = 0;
+	float r = 0; //used to determine a random direction choice
+	float s = 0; //used to determine how many times the object will move in the random direction, this is also random
 	float x;
 	float y;
-	int i;
+	float radius = 1;
 	float STARTX;
 	float STARTY;
 	Animator animator;
@@ -31,20 +31,20 @@ public class blobMove : MonoBehaviour {
 		}
 
 		animator.SetFloat("Speed", 0);
-		if(r > .20 && r < .4 && y < STARTY + 1){
+		if(r > .20 && r < .4 && y < STARTY + radius){
 			transform.Translate(0,speed,0);
 			animator.SetFloat("Speed", 1);
 		}
-		if(r >= .4 && r < .6 && y > STARTY - 1){
+		if(r >= .4 && r < .6 && y > STARTY - radius){
 			transform.Translate(0,(-1)*speed,0);
 			animator.SetFloat("Speed", 1);
 		}
-		if(r >= .6 && r < .8 && x > STARTX - 1){
+		if(r >= .6 && r < .8 && x > STARTX - radius){
 			transform.localScale = (new Vector3(1,1,1));
 			transform.Translate((-1)*speed,0,0);
 			animator.SetFloat("Speed", 1);
 		}
-		if(r >= .8 && x < STARTX + 1){
+		if(r >= .8 && x < STARTX + radius){
 			transform.localScale = (new Vector3(-1,1,1));
 			transform.Translate(speed,0,0);
 			animator.SetFloat("Speed", 1);
