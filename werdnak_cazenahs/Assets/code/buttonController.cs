21289dd5d610070ddc437fc7746748fe0476d12d
enahsthezombie <enahsnalob@gmail.com>
2015-03-16 15:39:38 -0500

    added button funtionality & updated door opening
    this includes add animations for buttons and doors

---
+++ werdnak_cazenahs/Assets/code/buttonController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonController : MonoBehaviour {
+	
+	Animator animator;
+	public bool buttonDown = false;
+	public doorHandler doorHand;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(){
+		buttonDown = true;
+		animator.SetBool ("ButtonDown", buttonDown);
+		doorHand.OpenDoor ();
+	}
+	void OnTriggerExit2D(){
+		buttonDown = false;
+		animator.SetBool ("ButtonDown", buttonDown);
+		doorHand.CloseDoor ();
+	}
+
+}
