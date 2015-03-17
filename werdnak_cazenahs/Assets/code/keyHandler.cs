71123b9821a695b9f91356a34db2b85e9ab9d390
Schwarzy1 <Schwarzyz@gmail.com>
2015-03-17 16:23:43 -0500

    Keys cannot be picked up by Enemies

--- werdnak_cazenahs/Assets/code/keyHandler.cs
+++ werdnak_cazenahs/Assets/code/keyHandler.cs
@@ -10,10 +10,19 @@ public class keyHandler : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
-		if(keyUI.transform.position.x == 100){
-			keyUI.transform.position = new Vector3(5,-3.2f,-1);
-			Destroy (gameObject);
+		if (other.gameObject.name.Equals ("Caz") ){
+			if(keyUI.transform.position.x == 100){
+				keyUI.transform.position = new Vector3(5,-3.2f,-1);
+				Destroy (gameObject);
+			}
 		}
+		if(other.gameObject.name.Equals ("Werd") && other.gameObject.GetComponent<werdnakMove>().attacking == false) {
+			if(keyUI.transform.position.x == 100){
+				keyUI.transform.position = new Vector3(5,-3.2f,-1);
+				Destroy (gameObject);
+			}
+		}
+
 	}
 
 }
