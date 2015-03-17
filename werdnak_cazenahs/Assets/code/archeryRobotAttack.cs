296ef78e24eb4a60ec13018bb8aa3bd94d1875e0
zboone1995 <zboone1995@gmail.com>
2015-03-17 15:40:42 -0500

    Archery Robot Prefab and some functionality

---
+++ werdnak_cazenahs/Assets/code/archeryRobotAttack.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class archeryRobotAttack : MonoBehaviour {
+	GameObject werd;
+	GameObject caz;
+	float cazPosX;
+	float cazPosY;
+	float cazDist;
+	float werdPosX;
+	float werdPosY;
+	float werdDist;
+	float posX;
+	float posY;
+	public int coolDownWait = 0;
+	public float radius = 5;
+	GameObject lazer;
+	Vector3 loc;
+	Quaternion quat = new Quaternion(0,0,0,0);
+	// Use this for initialization
+	void Start () {
+		caz = GameObject.Find ("Caz");
+		werd = GameObject.Find ("Werd");
+		posX = transform.position.x;
+		posY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cazPosX = caz.transform.position.x;
+		cazPosY = caz.transform.position.y;
+		cazDist = cazPosX * cazPosY;
+		werdPosX = werd.transform.position.x;
+		werdPosY = werd.transform.position.y;
+		werdDist = werdPosX * werdPosY;
+
+		if (cazPosX < posX + radius && cazPosX > posX - radius && cazPosY < posY + radius && cazPosY > posY - radius && cazDist < werdDist) {
+			if (coolDownWait <= 0) {
+				loc = new Vector3(transform.position.x+1, transform.position.y-0.1f, transform.position.z);
+				lazer = GameObject.Find("lazer_green");
+				Instantiate(lazer, loc , quat);
+				coolDownWait = 30;
+			}
+
+			if (coolDownWait > 0) {
+				coolDownWait--;
+			}
+
+		}
+		if (werdPosX < posX + radius && werdPosX > posX - radius && werdPosY < posY + radius && werdPosY > posY - radius && cazDist > werdDist) {
+			if (coolDownWait <= 0) {
+				loc = new Vector3(transform.position.x+1, transform.position.y-0.1f, transform.position.z);
+				lazer = GameObject.Find("lazer_green");
+				Instantiate(lazer, loc , quat);
+				coolDownWait = 30;
+			}
+
+			if (coolDownWait > 0) {
+				coolDownWait--;
+			}
+
+		}
+		/*
+		if (coolDownWait <= 0) {
+			loc = new Vector3(transform.position.x+1, transform.position.y-0.1f, transform.position.z);
+			lazer = GameObject.Find("lazer_green");
+			Instantiate(lazer, loc , quat);
+			coolDownWait = 30;
+		}
+		if (coolDownWait > 0) {
+			coolDownWait--;
+		}
+		*/
+
+	}
+}
---
+++ werdnak_cazenahs/Assets/code/enemyLazerMove.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyLazerMove : MonoBehaviour {
+
+	float direction;
+	GameObject robot;
+	float distance;
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreLayerCollision(13,1,true);
+		Physics2D.IgnoreLayerCollision(13,8,true);
+		Physics2D.IgnoreLayerCollision(13,9,true);
+		Physics2D.IgnoreLayerCollision(13,10,true);
+		Physics2D.IgnoreLayerCollision(13,11,true);
+		Physics2D.IgnoreLayerCollision(13,13,true);
+		Physics2D.IgnoreLayerCollision(13,14,true);
+		robot = GameObject.Find ("archeryRobot");
+		direction = 1f;
+		distance = 3.2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction == -1f) {
+			transform.Translate (-0.1f, 0, 0);
+		} else if (direction == 1f) {
+			transform.Translate (0.1f, 0, 0);
+		} 
+		if (Vector3.Distance (transform.position, robot.transform.position) >= distance && Vector3.Distance (transform.position, robot.transform.position) < 20) {
+			Destroy(gameObject);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.name.Equals("Werd") | other.gameObject.name.Equals("Caz")){
+			other.gameObject.GetComponent<heartsHandler>().TakeDamage(1);
+		}
+		Destroy(gameObject);
+	}
+}
