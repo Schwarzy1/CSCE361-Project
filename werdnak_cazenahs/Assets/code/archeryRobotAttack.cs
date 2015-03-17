bc88460e2a8793c6c5fe92f86dbf0d11122e9849
enahsthezombie <enahsnalob@gmail.com>
2015-03-17 17:38:16 -0500

    implemented rest of lazer robot

--- werdnak_cazenahs/Assets/code/archeryRobotAttack.cs
+++ werdnak_cazenahs/Assets/code/archeryRobotAttack.cs
@@ -10,13 +10,13 @@ public class archeryRobotAttack : MonoBehaviour {
 	float werdPosX;
 	float werdPosY;
 	float werdDist;
+	Quaternion quat = new Quaternion(0,0,0,0);
 	float posX;
 	float posY;
 	public int coolDownWait = 0;
 	public float radius = 5;
 	GameObject lazer;
 	Vector3 loc;
-	Quaternion quat = new Quaternion(0,0,0,0);
 	// Use this for initialization
 	void Start () {
 		caz = GameObject.Find ("Caz");
@@ -34,11 +34,29 @@ public class archeryRobotAttack : MonoBehaviour {
 		werdPosY = werd.transform.position.y;
 		werdDist = Vector3.Distance (transform.position, werd.transform.position);
 
-		if ((cazPosX < posX + radius && cazPosX > posX - radius && cazPosY < posY + radius && cazPosY > posY - radius && cazDist < werdDist)
-		    || (werdPosX < posX + radius && werdPosX > posX - radius && werdPosY < posY + radius && werdPosY > posY - radius && cazDist > werdDist)
-		    ) {
+		if (cazPosX < posX + radius && cazPosX > posX - radius && cazPosY < posY + radius && cazPosY > posY - radius && cazDist < werdDist){
+			if(cazPosX < posX){
+				transform.localScale = (new Vector3(1,1,1));
+			}else if (cazPosX > posX){
+				transform.localScale = (new Vector3(-1,1,1));
+			}
 			if (coolDownWait <= 0) {
-				loc = new Vector3(transform.position.x+1, transform.position.y-0.1f, transform.position.z);
+				loc = new Vector3(transform.position.x, transform.position.y-0.3f, transform.position.z-0.5f);
+				lazer = GameObject.Find("lazer_green");
+				Instantiate(lazer, loc , quat);
+				coolDownWait = 30;
+			}
+			if (coolDownWait > 0) {
+				coolDownWait--;
+			}
+		}if(werdPosX < posX + radius && werdPosX > posX - radius && werdPosY < posY + radius && werdPosY > posY - radius && cazDist > werdDist){
+			if(werdPosX < posX){
+				transform.localScale = (new Vector3(1,1,1));
+			}else if (werdPosX > posX){
+				transform.localScale = (new Vector3(-1,1,1));
+			}
+			if (coolDownWait <= 0) {
+				loc = new Vector3(transform.position.x, transform.position.y-0.3f, transform.position.z-0.5f);
 				lazer = GameObject.Find("lazer_green");
 				Instantiate(lazer, loc , quat);
 				coolDownWait = 30;
--- werdnak_cazenahs/Assets/code/enemyLazerMove.cs
+++ werdnak_cazenahs/Assets/code/enemyLazerMove.cs
@@ -5,26 +5,28 @@ public class enemyLazerMove : MonoBehaviour {
 
 	float direction;
 	GameObject robot;
+	heartsHandler hh;
 	float distance;
 	// Use this for initialization
 	void Start () {
-		Physics2D.IgnoreLayerCollision(13,1,true);
-		Physics2D.IgnoreLayerCollision(13,8,true);
-		Physics2D.IgnoreLayerCollision(13,9,true);
-		Physics2D.IgnoreLayerCollision(13,10,true);
-		Physics2D.IgnoreLayerCollision(13,11,true);
-		Physics2D.IgnoreLayerCollision(13,13,true);
-		Physics2D.IgnoreLayerCollision(13,14,true);
+		Physics2D.IgnoreLayerCollision(17,1,true);
+		Physics2D.IgnoreLayerCollision(17,9,true);
+		Physics2D.IgnoreLayerCollision(17,10,true);
+		Physics2D.IgnoreLayerCollision(17,13,true);
+		Physics2D.IgnoreLayerCollision(17,14,true);
+		Physics2D.IgnoreLayerCollision(17,15,true);
+		Physics2D.IgnoreLayerCollision(17,16,true);
 		robot = GameObject.Find ("arecheryRobot");
-		direction = 1f;
+		hh = GameObject.Find("hearts").GetComponent<heartsHandler>();
+		direction = robot.GetComponent<archeryRobotAttack> ().transform.localScale.x;
 		distance = 3.2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (direction == -1f) {
+		if (direction == 1f) {
 			transform.Translate (-0.1f, 0, 0);
-		} else if (direction == 1f) {
+		} else if (direction == -1f) {
 			transform.Translate (0.1f, 0, 0);
 		} 
 		if (Vector3.Distance (transform.position, robot.transform.position) >= distance && Vector3.Distance (transform.position, robot.transform.position) < 20) {
@@ -33,8 +35,8 @@ public class enemyLazerMove : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter2D(Collider2D other){
-		if(other.gameObject.name.Equals("Werd") | other.gameObject.name.Equals("Caz")){
-			other.gameObject.GetComponent<heartsHandler>().TakeDamage(1);
+		if(other.gameObject.name.Equals("Werd") || other.gameObject.name.Equals("Caz")){
+			hh.TakeDamage(1);
 		}
 		Destroy(gameObject);
 	}
