0cae7e8af01776a33fa5036a8c10a33b78239563
zboone1995 <zboone1995@gmail.com>
2015-03-18 15:30:10 -0500

    Fixed enemy adjacency issue

--- werdnak_cazenahs/Assets/code/blobMove.cs
+++ werdnak_cazenahs/Assets/code/blobMove.cs
@@ -12,12 +12,31 @@ public class blobMove : MonoBehaviour {
 	float STARTY;
 	bool isOne=false;
 	bool isTwo=false;
+	bool adjacent = false;
 	 public doorHandler doorhand;
 	Animator animator;
+
+
+
+	float cazPosX;
+	float cazPosY;
+	float cazDist;
+	float werdPosX;
+	float werdPosY;
+	float werdDist;
+	float posX;
+	float posY;
+
+
+	GameObject caz;
+	GameObject werd;
+
 	// Use this for initialization
 	public void Start () {
 		STARTX = transform.position.x;
 		STARTY = transform.position.y;
+		caz = GameObject.Find ("Caz");
+		werd = GameObject.Find ("Werd");
 		animator = GetComponent<Animator>();
 		if(this.name==("blob 1")){
 			this.
@@ -43,41 +62,116 @@ public class blobMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (adjacent == false) {
+			Move ();
+		} else {
+			AdjacentMove();
+		}
+		//GetComponent<Rigidbody2D> ().velocity = new Vector3 (1, 1, 1);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.name.Equals ("Caz") | other.gameObject.name.Equals ("Werd") ){
+			adjacent = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D other){
+		if (other.gameObject.name.Equals ("Caz") | other.gameObject.name.Equals ("Werd") ){
+			adjacent = false;
+		}
+	}
+
+	void Move(){
+
 		x = transform.position.x;
 		y = transform.position.y;
-
+		
 		if (s <= 0) {
 			r = Random.value;
 		}
 		if (s <= 0) {
 			s = Random.value * 100;
 		}
-
-		animator.SetFloat("Speed", 0);
-		if(r > .20 && r < .4 && y < STARTY + radius){
-			transform.Translate(0,speed,0);
-			animator.SetFloat("Speed", 1);
+		
+		animator.SetFloat ("Speed", 0);
+		if (r > .20 && r < .4 && y < STARTY + radius) {
+			transform.Translate (0, speed, 0);
+			animator.SetFloat ("Speed", 1);
 		}
-		if(r >= .4 && r < .6 && y > STARTY - radius){
-			transform.Translate(0,(-1)*speed,0);
-			animator.SetFloat("Speed", 1);
+		if (r >= .4 && r < .6 && y > STARTY - radius) {
+			transform.Translate (0, (-1) * speed, 0);
+			animator.SetFloat ("Speed", 1);
 		}
-		if(r >= .6 && r < .8 && x > STARTX - radius){
-			transform.localScale = (new Vector3(1,1,1));
-			transform.Translate((-1)*speed,0,0);
-			animator.SetFloat("Speed", 1);
+		if (r >= .6 && r < .8 && x > STARTX - radius) {
+			transform.localScale = (new Vector3 (1, 1, 1));
+			transform.Translate ((-1) * speed, 0, 0);
+			animator.SetFloat ("Speed", 1);
 		}
-		if(r >= .8 && x < STARTX + radius){
-			transform.localScale = (new Vector3(-1,1,1));
-			transform.Translate(speed,0,0);
-			animator.SetFloat("Speed", 1);
+		if (r >= .8 && x < STARTX + radius) {
+			transform.localScale = (new Vector3 (-1, 1, 1));
+			transform.Translate (speed, 0, 0);
+			animator.SetFloat ("Speed", 1);
 		}
-
+		
+		
 		s--;
-		//GetComponent<Rigidbody2D> ().velocity = new Vector3 (1, 1, 1);
-
 	}
 
-	
+	void AdjacentMove(){
+
+		x = transform.position.x;
+		y = transform.position.y;
+		cazPosX = caz.transform.position.x;
+		cazPosY = caz.transform.position.y;
+		cazDist = Vector3.Distance (transform.position, caz.transform.position);
+		werdPosX = werd.transform.position.x;
+		werdPosY = werd.transform.position.y;
+		werdDist = Vector3.Distance (transform.position, werd.transform.position);
+		posX = transform.position.x;
+		posY = transform.position.y;
+
+		if (cazDist < werdDist) {
+			if (cazPosY > posY) {
+				transform.Translate (0, speed, 0);
+				animator.SetFloat("Speed", 1);
+			}
+			if (cazPosY < posY) {
+				transform.Translate (0, (-1) * speed, 0);
+				animator.SetFloat("Speed", 1);
+			}
+			if (cazPosX < posX) {
+				transform.localScale = (new Vector3 (1, 1, 1));
+				transform.Translate ((-1) * speed, 0, 0);
+				animator.SetFloat("Speed", 1);
+			}
+			if (cazPosX > posX) {
+				transform.localScale = (new Vector3 (-1, 1, 1));
+				transform.Translate (speed, 0, 0);
+				animator.SetFloat("Speed", 1);
+			}
+		}
+		if (cazDist > werdDist) {
+			if (werdPosY > posY) {
+				transform.Translate (0, speed, 0);
+				animator.SetFloat("Speed", 1);
+			}
+			if (werdPosY < posY) {
+				transform.Translate (0, (-1) * speed, 0);
+				animator.SetFloat("Speed", 1);
+			}
+			if (werdPosX < posX) {
+				transform.localScale = (new Vector3 (1, 1, 1));
+				transform.Translate ((-1) * speed, 0, 0);
+				animator.SetFloat("Speed", 1);
+			}
+			if (werdPosX > posX) {
+				transform.localScale = (new Vector3 (-1, 1, 1));
+				transform.Translate (speed, 0, 0);
+				animator.SetFloat("Speed", 1);
+			}
+		}
+	}
 
 }
--- werdnak_cazenahs/Assets/code/damageHandler.cs
+++ werdnak_cazenahs/Assets/code/damageHandler.cs
@@ -4,22 +4,31 @@ using System.Collections;
 public class damageHandler : MonoBehaviour {
 	heartsHandler hh;
 	public int damage = 1;
+	public float coolDown;
 	// Use this for initialization
 	void Start () {
 		hh = GameObject.Find("hearts").GetComponent<heartsHandler>();
+		coolDown = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		coolDown--;
 	}
 
-	void OnCollisionEnter2D(Collision2D other){
-		if (other.gameObject.name.Equals ("Caz") ){
-			hh.TakeDamage(damage);
-		}
-		if(other.gameObject.name.Equals ("Werd") && other.gameObject.GetComponent<werdnakMove>().attacking == false) {
-			hh.TakeDamage(damage);
-		}
+	void OnCollisionStay2D(Collision2D other){
+		if (coolDown <= 0) {
+			if (other.gameObject.name.Equals ("Caz")) {
+				hh.TakeDamage (damage);
+			}
+			if (other.gameObject.name.Equals ("Werd") && other.gameObject.GetComponent<werdnakMove> ().attacking == false) {
+				hh.TakeDamage (damage);
+			}
+			coolDown = 60;
+		} 
 	}
+	void OnCollisionExit2D(Collision2D other){
+		coolDown = 0;
+	}
+
 }
