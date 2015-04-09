d73713c23911a36e9506dab7857389c693bfef0d
enahsthezombie <enahsnalob@gmail.com>
2015-04-09 15:10:09 -0500

    fixed not able to have many robots
    updated enemylazermove to fix this

--- werdnak_cazenahs/Assets/code/enemyLazerMove.cs
+++ werdnak_cazenahs/Assets/code/enemyLazerMove.cs
@@ -6,7 +6,15 @@ public class enemyLazerMove : MonoBehaviour {
 	float direction;
 	GameObject robot;
 	heartsHandler hh;
+	GameObject werd;
+	GameObject caz;
 	float distance;
+	float cazPosX;
+	float cazDist;
+	float werdPosX;
+	float werdDist;
+	float posX;
+	Vector3 pos;
 	// Use this for initialization
 	void Start () {
 		Physics2D.IgnoreLayerCollision(17,1,true);
@@ -16,22 +24,43 @@ public class enemyLazerMove : MonoBehaviour {
 		Physics2D.IgnoreLayerCollision(17,14,true);
 		Physics2D.IgnoreLayerCollision(17,15,true);
 		Physics2D.IgnoreLayerCollision(17,16,true);
-		robot = GameObject.Find ("arecheryRobot");
+		caz = GameObject.Find ("Caz");
+		werd = GameObject.Find ("Werd");
+		pos = transform.position;
+		posX = transform.position.x;
+		cazPosX = caz.transform.position.x;
+		cazDist = Vector3.Distance (transform.position, caz.transform.position);
+		werdPosX = werd.transform.position.x;
+		werdDist = Vector3.Distance (transform.position, werd.transform.position);
 		hh = GameObject.Find("hearts").GetComponent<heartsHandler>();
-		direction = robot.GetComponent<archeryRobotAttack> ().transform.localScale.x;
+		if (cazDist < werdDist) {
+			if (cazPosX < posX) {
+				direction = -0.1f;
+			} else if (cazPosX > posX) {
+				direction = 0.1f;
+			}
+			
+		}if (cazDist > werdDist) {
+			if (werdPosX < posX) {
+				direction = -0.1f;
+			} else if (werdPosX > posX) {
+				direction = 0.1f;
+			}
+
+		}
 		distance = 3.2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (direction == 1f) {
-			transform.Translate (-0.1f, 0, 0);
-		} else if (direction == -1f) {
-			transform.Translate (0.1f, 0, 0);
-		} 
-		if (robot!=null&& Vector3.Distance ( transform.position, robot.transform.position) >= distance && Vector3.Distance (transform.position, robot.transform.position) < 20) {
-			Destroy(gameObject);
+		if(!(-9900 > posX && posX > -10100)) {
+			transform.Translate (direction, 0, 0);
+			
+			if (Vector3.Distance ( transform.position, pos) >= distance) {
+				Destroy(gameObject);
+			}
 		}
+
 	}
 	
 	void OnTriggerEnter2D(Collider2D other){
