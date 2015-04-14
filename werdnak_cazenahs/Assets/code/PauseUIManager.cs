d81e311814b0429c82574fca374c908157860eb4
Schwarzy1 <Schwarzyz@gmail.com>
2015-04-14 10:08:02 -0500

    Pause menu disabled when P is pressed

--- werdnak_cazenahs/Assets/code/PauseUIManager.cs
+++ werdnak_cazenahs/Assets/code/PauseUIManager.cs
@@ -6,6 +6,7 @@ public class PauseUIManager : MonoBehaviour {
 	// Use this for initialization
 	GameObject canvas;
 	public static bool paused=false;
+	public int cooldown=0;
 	void Start(){
 		paused = false;
 		canvas = GameObject.Find ("Canvas");
@@ -15,13 +16,18 @@ public class PauseUIManager : MonoBehaviour {
 	// Update is called once per frame
 
 	void FixedUpdate(){
-		if (Input.GetKey (KeyCode.P)) {
+		if (Input.GetKey (KeyCode.P)&& cooldown<=0&& paused==false) {
 			//Pause Menu stuff
-			
 			canvas.SetActive (true);
 			paused = true;
-			
+			cooldown=30;
 		} 
+		if(Input.GetKey (KeyCode.P)&& cooldown<=0&& paused==true){
+			canvas.SetActive (false);
+			paused = false;
+			cooldown=30;
+		}
+		cooldown--;
 		
 	}
 }
